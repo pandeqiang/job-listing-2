ad03469ce35adcdca5ae2137d1a9de6cd7bbd745
pandeqiang <ikilio99@gmail.com>
2017-04-26 09:43:32 +0800

    implement step 5

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   resources :jobs
   namespace :admin do
     resources :jobs do
+      member do
+        post :publish
+        post :hide
+      end
   end
 end
 
